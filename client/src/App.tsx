c0e6cd4cd5db05fc0bee43107b57ce6f66212e08
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-01 00:40:50 +0100

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080");
+    console.log(response.data.fruits);
+  };
 
   return (
     <>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
