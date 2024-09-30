e431c954131eecc998464a62f2a4053b14585b32
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-30 23:59:52 +0100

    server

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-  origin: {"http:localhost:5173"}
-}
+  origin: ["http://localhost:5173"],
+};
 // route
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "berry", "mango"] });
